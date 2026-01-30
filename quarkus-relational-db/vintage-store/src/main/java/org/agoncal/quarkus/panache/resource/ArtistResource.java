0404ed6c16bbdff2e3db03742a2b2e33560ef8a8
Oscar Molina <omolinab@redhat.com>
2026-01-30 12:54:10 +0100

    adding import.sql and ArtistResource

---
+++ quarkus-relational-db/vintage-store/src/main/java/org/agoncal/quarkus/panache/resource/ArtistResource.java
@@ -0,0 +1,36 @@
+package org.agoncal.quarkus.panache.resource;
+
+import java.util.List;
+
+import org.agoncal.quarkus.jdbc.Artist;
+import org.agoncal.quarkus.panache.repository.ArtistRepository;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/api/artists")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class ArtistResource {
+
+    @Inject
+    ArtistRepository repository;
+
+    @GET
+    @Path("{id}")
+    public Artist findArtistById(@PathParam("id") Long id) {
+        return repository.findByIdOptional(id).orElseThrow(NotFoundException::new);
+    }
+
+    @GET
+    public List<Artist> listAllArtists() {
+        return repository.listAll();
+    }
+
+}
