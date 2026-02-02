efc7cb7d3b6c3d328ec8b70f8c65468d9e012d84
Oscar Molina <omolinab@redhat.com>
2026-02-02 13:08:57 +0100

    adding transaction for Publishers

--- quarkus-relational-db/vintage-store/src/main/java/org/agoncal/quarkus/panache/resource/PublisherResource.java
+++ quarkus-relational-db/vintage-store/src/main/java/org/agoncal/quarkus/panache/resource/PublisherResource.java
@@ -4,17 +4,26 @@ import java.util.List;
 
 import org.agoncal.quarkus.panache.model.Publisher;
 
+import jakarta.transaction.Transactional;
+import jakarta.transaction.Transactional.TxType;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriBuilder;
+import jakarta.ws.rs.core.UriInfo;
 
 @Path("/api/publishers")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
+@Transactional(TxType.SUPPORTS)
 public class PublisherResource {
 
     @GET
@@ -27,5 +36,21 @@ public class PublisherResource {
     public List<Publisher> listAllPublishers() {
         return Publisher.listAll();
     }
+
+    @Transactional(TxType.REQUIRED)
+    @POST
+    public Response persistPublisher(Publisher publisher, @Context UriInfo uriInfo) {
+        Publisher.persist(publisher);
+        UriBuilder builder = uriInfo.getAbsolutePathBuilder().path(Long.toString(publisher.id));
+        return Response.created(builder.build()).build();
+    }
+
+    @Transactional
+    @DELETE
+    @Path("{id}")
+    public void deletePublisher(@PathParam("id") Long id) {
+        Publisher.deleteById(id);
+    }
+
 
 }
