8772b96018e846b73df8e0ce328578395b368337
Oscar Molina <omolinab@redhat.com>
2026-02-02 12:58:05 +0100

    adding POST and DELETE methods for artist

--- quarkus-relational-db/vintage-store/src/main/java/org/agoncal/quarkus/panache/resource/ArtistResource.java
+++ quarkus-relational-db/vintage-store/src/main/java/org/agoncal/quarkus/panache/resource/ArtistResource.java
@@ -7,12 +7,18 @@ import org.agoncal.quarkus.panache.repository.ArtistRepository;
 
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriBuilder;
+import jakarta.ws.rs.core.UriInfo;
 
 @Path("/api/artists")
 @Produces(MediaType.APPLICATION_JSON)
@@ -31,6 +37,19 @@ public class ArtistResource {
     @GET
     public List<Artist> listAllArtists() {
         return repository.listAllArtistsSorted();
+    }
+
+    @POST
+    public Response persistArtist(Artist artist, @Context UriInfo uriInfo) {
+        repository.persist(artist);
+        UriBuilder builder = uriInfo.getAbsolutePathBuilder().path(Long.toString(artist.getId()));
+        return Response.created(builder.build()).build();
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void deleteArtist(@PathParam("id") Long id) {
+        repository.deleteById(id);
     }
 
 }
